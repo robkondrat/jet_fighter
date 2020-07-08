02cf1ac8f701c2fca98dfbdde67ed60abf94e4aa
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-07-08 10:47:14 -0500

    Jet directions and boundaries

--- Jet.js
+++ Jet.js
@@ -1,11 +1,41 @@
 class Jet {
   constructor(image) {
     this.x = random(width);
-    this.y = random(height):
+    this.y = random(height);
     this.image = image;
   }
 
+  update() {
+    this.goTheWayWereFacing();
+    this.constrainToMap();
+  }
+
+  goTheWayWereFacing() {
+    this.x += this.speed * sin(this.angle);
+    this.y += this.speed * cos(this.angle);
+  }
+
+  constrainToMap() {
+    if (this.x < -this.image.width) {
+      this.x = width;
+    } else if (this.x > width) {
+      this.x = 0;
+    }
+
+    if (this.y > height) {
+      this.y = 0;
+    } else if (this.y < -this.image.height) {
+      this.y = height;
+    }
+  }
+
   draw() {
-    image(this.image, this.x, this.y);
+    push();
+    translate(this.x, this.y);
+    imageMode(CENTER);
+    rotate(this.angle + HALF_PI);
+
+    image(this.image, 0, 0);
+    pop();
   }
 }
